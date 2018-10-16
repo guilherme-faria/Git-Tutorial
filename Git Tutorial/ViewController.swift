c79e4a99b1694aaa1d33c50cd2d7ce9dbc60ea5a
Guilherme Faria <guilherme.faria@ffptn1923.local>
2018-10-16 14:21:17 +0100

    Reverted the changes

--- Git Tutorial/ViewController.swift
+++ Git Tutorial/ViewController.swift
@@ -11,10 +11,9 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        print("Hello World!")
-        print("Hello Guilherme!")
         super.viewDidLoad()
-        }
+        // Do any additional setup after loading the view, typically from a nib.
+    }
 
 
 }
