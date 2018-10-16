96140b3d928fc1b4d7934be066819627979116a2
Guilherme Faria <guilherme.faria@ffptn1923.local>
2018-10-16 14:10:24 +0100

    Initial Commit

---
+++ Git Tutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git Tutorial
+//
+//  Created by Guilherme Faria on 16/10/2018.
+//  Copyright © 2018 Guilherme Faria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
